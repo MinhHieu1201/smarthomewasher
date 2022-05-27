be46045a50d3c2bb49583d0b50ed63846092f870
MinhHieu1201 <nguyenminhhieu12a@gmail.com>
2022-05-27 10:33:47 +0000

    ad

--- washer-done/functions/crawl.js
+++ washer-done/functions/crawl.js
@@ -6,7 +6,7 @@ async function getAll() {
     let data = [],
         page = 1;
     for (page = 1; page <= 10;) {
-        await request('https://yomaps.net/vi/VN/24-ha-noi/type/68-pharmacy?page=' + page.toString(), (error, response, html) => {
+        await request('https://yomaps.net/vi/VN/24-ha-noi/type/28-dentist?page=' + page.toString(), (error, response, html) => {
             if (!error && response.statusCode == 200) {
                 let $ = cheerio.load(html);
                 // data = $('.block-grid-v2-info').find('.text-ellipsis a').attr('href');
@@ -26,12 +26,12 @@ async function getDetail(url) {
     await request('https://yomaps.net' + url.toString(), (error, response, htmlDetail) => {
         if (!error && response.statusCode == 200) {
             let $ = cheerio.load(htmlDetail);
-            let type = $('tr:eq(0)').find('td:eq(1)').text();
-            let store = $('h1').text();
-            let address = $('tr:eq(1)').find('td:eq(1)').text();
-            let location = $('tr:eq(2)').find('td:eq(1)').find('a').text();
-            let phone = $('tr:eq(3)').find('td:eq(1)').text();
-            let site = $('tr:eq(4)').find('td:eq(1)').text();
+            let type = $('tr:eq(0)').find('td:eq(1)').text().trim();
+            let store = $('h1').text().trim().replace("Bác sỹ nha khoa: ", "");
+            let address = $('tr:eq(1)').find('td:eq(1)').text().trim();
+            let location = $('tr:eq(2)').find('td:eq(1)').find('a').text().trim().replace("  -  Xem vị trí trên bản đồ","");
+            let phone = $('tr:eq(3)').find('td:eq(1)').text().trim();
+            let site = $('tr:eq(4)').find('td:eq(1)').text().trim();
             let img = "";
             $('.fancybox > img').each((i, it) => {
                 if (it && it.attribs && it.attribs.src) {
@@ -60,19 +60,10 @@ function main() {
         res.forEach(item => {
             getDetail(item).then((res) => {
                 result.push(res);
-                // fs.writeFileSync('pamacy.json', JSON.stringify(result)); // lưu dữ liệu vào file data.json
+                fs.writeFileSync('dentist.json', JSON.stringify(result)); // lưu dữ liệu vào file data.json
             });
         })
     });
-    let binaryWS = XLSX.utils.json_to_sheet(result);
-
-    // Create a new Workbook
-    var wb = XLSX.utils.book_new()
-
-    // Name your sheet
-    XLSX.utils.book_append_sheet(wb, binaryWS, 'sheet1')
-
-    // export your excel
-    XLSX.writeFile(wb, 'paramcy.xlsx');
+   
 }
 main()
